d70574edad5a59de6691ecb4bdfa15fff2bc2f61
KEEBUM KIM <jimmy@KEEBUMui-MacBookPro.local>
2024-04-06 14:53:08 +0900

    disableHostCheck true 추가

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   runtimeCompiler: true,
   transpileDependencies: [ansiRegex],
   devServer: {
-    port: process.env.VUE_APP_PORT
+    port: process.env.VUE_APP_PORT,
+    disableHostCheck: true
   }
 };
