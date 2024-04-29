ac468e811bb2f581d31857be273c7fc5ff080fe7
송주은 <songzuen@bitcube.co.kr>
2024-04-29 16:05:02 +0900

    아이디/비밀번호 찾기 문구 수정

--- src/components/IdSearchPop.vue
+++ src/components/IdSearchPop.vue
@@ -8,18 +8,18 @@
           <h2 class="modalTitle">아이디 찾기</h2>
           <div class="modalTopBox">
             <ul>
-              <li><div>아래 사업자등록번호, 찾고자 하는 로그인 사용자명 그리고 등록된 이메일을 정확인 입력하셔야 이메일로 아이디를 발송합니다.</div></li>
+              <li><div>아래 사업자등록번호, 찾고자 하는 로그인 사용자명 그리고 등록된 이메일을 정확히 입력하셔야 이메일 및 문자로 아이디를 발송합니다.</div></li>
               <li><div>[아이디 찾기]는 전자입찰 협력사 사용자만 해당 됩니다. 계열사 사용자는 시스템 관리자에게 문의해 주십시오.</div></li>
             </ul>
           </div>
           <div class="flex align-items-center mt30">
             <div class="formTit flex-shrink0 width150px">사업자등록번호 <span class="star">*</span></div>
             <div class="flex align-items-center width100">
-              <input type="text" v-model="idSearch.regnum1" @keypress="onlyNumber" maxlength="3" class="inputStyle" placeholder="">
+              <input type="text" v-model="idSearch.regnum1" maxlength="3" class="inputStyle">
               <span style="margin:0 10px">-</span>
-              <input type="text" v-model="idSearch.regnum2" @keypress="onlyNumber" maxlength="2" class="inputStyle" placeholder="">
+              <input type="text" v-model="idSearch.regnum2" maxlength="2" class="inputStyle">
               <span style="margin:0 10px">-</span>
-              <input type="text" v-model="idSearch.regnum3" @keypress="onlyNumber" maxlength="5" class="inputStyle" placeholder="">
+              <input type="text" v-model="idSearch.regnum3" maxlength="5" class="inputStyle">
             </div>
           </div>
           <div class="flex align-items-center mt10">
@@ -31,7 +31,7 @@
           <div class="flex align-items-center mt10">
             <div class="formTit flex-shrink0 width150px">등록된 이메일 <span class="star">*</span></div>
             <div class="flex align-items-center width100">
-              <input type="text" v-model="idSearch.userEmail" class="inputStyle" placeholder="ex) sample@iljin.co.kr">
+              <input type="text" v-model.trim="idSearch.userEmail" class="inputStyle" placeholder="ex) sample@iljin.co.kr">
             </div>
           </div>
           <div class="modalFooter">
@@ -65,7 +65,7 @@
       <div class="modal-content">
         <div class="modal-body">
           <a href="#" class="ModalClose" data-dismiss="modal" title="닫기"><i class="fa-solid fa-xmark"></i></a>
-          <div class="alertText2">입력하신 이메일로 e-bidding 시스템에 접속하실 아이디를 전송합니다.<br>이메일로 아이디를 전송하시겠습니까?</div>
+          <div class="alertText2">입력하신 사용자에게 문자와 이메일로 e-bidding 시스템에 접속하실 아이디를 전송합니다.<br>아이디를 전송하시겠습니까?</div>
           <div class="modalFooter">
             <a href="#" class="modalBtnClose" data-dismiss="modal" title="취소">취소</a>
             <a href="#" @click.prevent="send" class="modalBtnCheck" title="전송">전송</a>
@@ -87,12 +87,21 @@ export default {
       idSearch: {},
     }
   },
+  watch : {
+    // 사업자 등록번호
+    'idSearch.regnum1'(){
+      this.idSearch.regnum1 = this.idSearch.regnum1.replace(/[^0-9]/g, '').trim();
+    },
+    'idSearch.regnum2'(){
+      this.idSearch.regnum2 = this.idSearch.regnum2.replace(/[^0-9]/g, '').trim();
+    },
+    'idSearch.regnum3'(){
+      this.idSearch.regnum3 = this.idSearch.regnum3.replace(/[^0-9]/g, '').trim();
+    }
+  },
   methods: {
     initModal() {
       this.idSearch = {};
-    },
-    onlyNumber(e) {
-      if (!/\d/.test(event.key) && event.key !== '.') return e.preventDefault();
     },
     validate() {  
       if (this.idSearch.regnum1 == null || this.idSearch.regnum2 == null || this.idSearch.regnum3 == null || this.idSearch.userName == null || this.idSearch.userEmail == null) {
@@ -111,7 +120,7 @@ export default {
           $("#idSearch").modal("hide");
           this.$swal({
             type: "info",
-            text: "이메일로 전송되었습니다."
+            text: "전송되었습니다."
           });
         } else {
           this.$swal({
--- src/components/PwSearchPop.vue
+++ src/components/PwSearchPop.vue
@@ -8,7 +8,7 @@
           <h2 class="modalTitle">비밀번호 찾기</h2>
           <div class="modalTopBox">
             <ul>
-              <li><div>아래 사업자등록번호, 찾고자 하는 로그인 사용자명, 로그인 아이디 그리고 등록된 이메일을 정확인 입력하셔야 이메일로 비밀번호를 발송합니다.</div></li>
+              <li><div>아래 사업자등록번호, 찾고자 하는 로그인 사용자명, 로그인 아이디 그리고 등록된 이메일을 정확히 입력하셔야 이메일 및 문자로 비밀번호를 발송합니다.</div></li>
               <li><div>비밀번호는 초기화되어 발송 하므로 로그인 후 암호를 변경하시고 사용하십시오.</div></li>
               <li><div>[비밀번호 찾기]는 전자입찰 협력사 사용자만 해당 됩니다. 계열사 사용자는 시스템 관리자에게 문의해 주십시오.</div></li>
             </ul>
@@ -16,11 +16,11 @@
           <div class="flex align-items-center mt30">
             <div class="formTit flex-shrink0 width150px">사업자등록번호 <span class="star">*</span></div>
             <div class="flex align-items-center width100">
-              <input type="text" v-model="pwSearch.regnum1" @keypress="onlyNumber" maxlength="3" class="inputStyle" placeholder="">
+              <input type="text" v-model="pwSearch.regnum1" maxlength="3" class="inputStyle">
               <span style="margin:0 10px">-</span>
-              <input type="text" v-model="pwSearch.regnum2" @keypress="onlyNumber" maxlength="2" class="inputStyle" placeholder="">
+              <input type="text" v-model="pwSearch.regnum2" maxlength="2" class="inputStyle">
               <span style="margin:0 10px">-</span>
-              <input type="text" v-model="pwSearch.regnum3" @keypress="onlyNumber" maxlength="5" class="inputStyle" placeholder="">
+              <input type="text" v-model="pwSearch.regnum3" maxlength="5" class="inputStyle">
             </div>
           </div>
           <div class="flex align-items-center mt10">
@@ -32,13 +32,13 @@
           <div class="flex align-items-center mt10">
             <div class="formTit flex-shrink0 width150px">로그인 아이디 <span class="star">*</span></div>
             <div class="flex align-items-center width100">
-              <input type="text" v-model="pwSearch.userId" class="inputStyle" placeholder="">
+              <input type="text" v-model.trim="pwSearch.userId" class="inputStyle" placeholder="">
             </div>
           </div>
           <div class="flex align-items-center mt10">
             <div class="formTit flex-shrink0 width150px">등록된 이메일 <span class="star">*</span></div>
             <div class="flex align-items-center width100">
-              <input type="text" v-model="pwSearch.userEmail" class="inputStyle" placeholder="ex) sample@iljin.co.kr">
+              <input type="text" v-model.trim="pwSearch.userEmail" class="inputStyle" placeholder="ex) sample@iljin.co.kr">
             </div>
           </div>
           <div class="modalFooter">
@@ -72,7 +72,7 @@
       <div class="modal-content">
         <div class="modal-body">
           <a href="#" class="ModalClose" data-dismiss="modal" title="닫기"><i class="fa-solid fa-xmark"></i></a>
-					<div class="alertText2">입력하신 이메일로 e-bidding 시스템에 접속하실 비밀번호를 전송합니다.<br>비밀번호는 초기화 되어 새로 생성됩니다.<br>이메일로 비밀번호를 전송하시겠습니까?</div>
+					<div class="alertText2">입력하신 사용자에게 문자와 이메일로 e-bidding 시스템에 접속하실 비밀번호를 전송합니다.<br>비밀번호는 초기화 되어 새로 생성됩니다.<br>비밀번호를 전송하시겠습니까?</div>
           <div class="modalFooter">
             <a href="#" class="modalBtnClose" data-dismiss="modal" title="취소">취소</a>
             <a href="#" @click.prevent="send" class="modalBtnCheck" title="전송">전송</a>
@@ -94,12 +94,21 @@ export default {
       pwSearch: {},
     }
   },
+  watch : {
+    // 사업자 등록번호
+    'pwSearch.regnum1'(){
+      this.pwSearch.regnum1 = this.pwSearch.regnum1.replace(/[^0-9]/g, '').trim();
+    },
+    'pwSearch.regnum2'(){
+      this.pwSearch.regnum2 = this.pwSearch.regnum2.replace(/[^0-9]/g, '').trim();
+    },
+    'pwSearch.regnum3'(){
+      this.pwSearch.regnum3 = this.pwSearch.regnum3.replace(/[^0-9]/g, '').trim();
+    }
+  },
   methods: {
     initModal() {
       this.pwSearch = {};
-    },
-    onlyNumber(event) {
-      if (!/\d/.test(event.key) && event.key !== '.') return event.preventDefault();
     },
     validate() {  
       if (this.pwSearch.regnum1 == null || this.pwSearch.regnum2 == null || this.pwSearch.regnum3 == null || this.pwSearch.userName == null || this.pwSearch.userEmail == null) {
@@ -118,7 +127,7 @@ export default {
           $("#pwSearch").modal("hide");
           this.$swal({
             type: "info",
-            text: "이메일로 전송되었습니다."
+            text: "전송되었습니다."
           });
         } else {
           this.$swal({
